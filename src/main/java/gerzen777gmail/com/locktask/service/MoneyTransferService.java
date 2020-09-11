ee89bd36cd00f8aeefc2bc06ee41ebf21f71a5f0
salvan88 <gerzen777@gmail.com>
2020-09-11 22:44:59 +0300

    Third iteration

--- src/main/java/gerzen777gmail/com/locktask/service/MoneyTransferService.java
+++ src/main/java/gerzen777gmail/com/locktask/service/MoneyTransferService.java
@@ -2,49 +2,52 @@ package gerzen777gmail.com.locktask.service;
 
 import gerzen777gmail.com.locktask.model.Account;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 
 public class MoneyTransferService {
 
-
     public void transfer(Account from, Account to, Long amount) {
 
         Lock first = new ReentrantLock();
         Lock second = new ReentrantLock();
-        Map<Lock, Long> locksMap = new HashMap<>();
+        Map<Long, Lock> locksMap = new ConcurrentHashMap<>();
 
         if (from.getId() > to.getId()) {
-            locksMap.put(first, from.getId());
-            locksMap.put(second, to.getId());
+            locksMap.put(from.getId(), first);
+            locksMap.put(to.getId(), second);
         } else {
-            locksMap.put(first, to.getId());
-            locksMap.put(second, from.getId());
+            locksMap.put(to.getId(), first);
+            locksMap.put(from.getId(), second);
         }
 
-        first.lock();
-        from.setId(locksMap.get(first));
+
+        locksMap.putIfAbsent(from.getId(),first).lock();
+
         try {
-        second.lock();
-            to.setId(locksMap.get(second));
+
+            locksMap.putIfAbsent(to.getId(), second).lock();
+
             try {
+
                 if (from.getBalance() >= amount) {
                     from.setBalance(from.getBalance() - amount);
                     to.setBalance(to.getBalance() + amount);
                 } else {
                     throw new IllegalArgumentException("Нет денег!");
                 }
+
             } catch (IllegalArgumentException e) {
                 e.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            first.unlock();
-            second.unlock();
+            locksMap.get(from.getId()).unlock();
+            locksMap.get(to.getId()).unlock();
         }
     }
 
